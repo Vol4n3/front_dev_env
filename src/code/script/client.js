7cb28aa7eab694c25c8c16b677c7ba07a592076d
Vol4n3 <jcvolan@gmail.com>
2018-02-14 18:31:10 +0100

    add material design

--- src/code/script/client.js
+++ src/code/script/client.js
@@ -1,5 +1,8 @@
 let $ = require('jquery');
-
+// require('popper.js');
+// require('bootstrap');
+require('material-design-lite');
 $(() => {
     //Code JS goes here
+    $('body').append('<p>Hello from Js</p>');
 });
